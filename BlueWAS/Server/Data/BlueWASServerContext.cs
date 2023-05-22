2805da67ca42a712458fc71094cfbf41f353bfc8
Test USer <testex@abc.com>
2023-05-22 23:01:41 +0500

    Add project files.

---
+++ BlueWAS/Server/Data/BlueWASServerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlueWAS.Shared;
+
+namespace BlueWAS.Server.Data
+{
+    public class BlueWASServerContext : DbContext
+    {
+        public BlueWASServerContext (DbContextOptions<BlueWASServerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlueWAS.Shared.Student> Student { get; set; } = default!;
+    }
+}
---
+++ BlueWAS/Shared/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueWAS.Shared
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(5, ErrorMessage = "name limit exceeds ", MinimumLength = 5)]
+        public string Name { get; set; }
+
+        public string RollNumber { get; set; }
+
+        public DateTime DOB { get; set; }
+
+
+
+    }
+}
